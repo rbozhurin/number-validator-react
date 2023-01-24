063c1572af5e91ebee795afa56e25dd248e2691a
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-24 20:55:21 +0200

    ddddd

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import React, { useMemo, useState } from "react";
 import "./App.css";
+import { useMemo, useState } from "react";
 
 function App() {
-  const [text, setText] = useState(null);
+  const [value, setValue] = useState("");
+  const [valid, setValid] = useState(false);
 
-  function check(v) {
-    let r = /^\d+$/;
-    if (text) {
-      if (v.match(r)) {
-        return true;
-      } else {
-        return false;
-      }
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    if (isNaN(newValue)) {
+      setValid(false);
+    } else {
+      setValid(true);
     }
+  };
+  const validationMessage = useMemo(() => {
+    return valid ? "Valid number" : "Invalid number";
+  }, [valid]);
+  if (validationMessage === "Valid number") {
+    document.getElementById("my-element").className = "fas fa-check";
   }
-
-  const boo = useMemo(() => {
-    return check(text);
-  }, [text]);
 
   return (
     <div className="App">
       <div className="control has-icons-right">
-        <input
-          className="input is-large"
-          type="text"
-          value={text}
-          placeholder="Enter number..."
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
+        <form>
+          <input
+            className="input is-large"
+            type="text"
+            value={value}
+            onChange={handleChange}
+            placeholder="Enter number..."
+          />
+        </form>
+
+        <div>
+          <p>{validationMessage}</p>
+        </div>
         <span className="icon is-small is-right">
-          <i className={boo ? "fas fa-check" : "fas fa-times"} />
+          <i className="fas fa-times" id="my-element" />
         </span>
       </div>
     </div>
