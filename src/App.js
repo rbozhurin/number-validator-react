f3ac16c28a5b5cef06d8bec2f7d94b6571c38ca7
Rayko Bozhurin <rbozhurin@gmail.com>
2023-01-24 20:26:35 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import "./App.css";
+import { useState, useMemo } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const isValid = useMemo(() => {
+    // Use a regular expression to check if the text is a valid number
+    return /^\d+$/.test(text);
+  }, [text]);
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={`fas ${isValid ? "fa-check" : "fa-times"}`} />
+        </span>
       </div>
     </div>
   );
